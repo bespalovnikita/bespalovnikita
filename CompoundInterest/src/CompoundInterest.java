994ab91985cfddf369866c9f4218cea8c5209111
Nick <bespalovnikk@Mail.ru>
2016-10-20 09:07:57 +0600

    fc

---
+++ CompoundInterest/src/CompoundInterest.java
@@ -0,0 +1,81 @@
+/**
+ @version 1.30 1999-12-16
+ @author Cay Horstmann
+ */
+
+import java.text.*;
+import javax.swing.*;
+
+public class CompoundInterest{
+
+    public static void main(String[] args)    {
+        String text = JOptionPane.showInputDialog
+                ("Write what u want");
+        String space="    ";
+        char open='{';
+        char close='}';
+        char bracket=';';
+        int opencount=0;
+        boolean inputclose=false;
+        boolean opened=false;
+        String retext="";
+        boolean nextstring=false;
+        boolean nextstring2=false;
+        for (int i=0; i< text.length(); i++){
+            if (nextstring){
+                retext = retext + "\n";
+                for (int j = 0; j < opencount; j++){
+                    retext = retext + space;
+                }
+            }
+
+            if (nextstring2) {
+                retext = retext + "\n";
+                for (int j = 0; j < opencount - 1; j++) {
+                    retext = retext + space;
+                }
+            }
+
+            inputclose=false;
+            nextstring=false;
+            nextstring2=false;
+
+            if(text.charAt(i)==open){
+                opencount= opencount +1;
+                opened=true;
+                nextstring=true;
+            }
+
+            if((text.charAt(i)==close)&&(opened)){
+                opencount = opencount - 1;
+                retext = retext + "\n";
+                inputclose = true;
+            }
+
+            if (opened){
+                if (inputclose)
+                {
+                    for (int j = 0; j < opencount; j++) {
+                        retext = retext + space;
+
+                    }
+                   nextstring2=true;
+                }
+                retext= retext+text.charAt(i);
+                if((text.charAt(i)==bracket)&&(opened)){
+                    retext = retext + "\n";
+                    for (int j = 0; j < opencount; j++) {
+                        retext = retext + space;
+                    }
+                }
+
+            }
+
+            if ((opened)&&(opencount==0)) {
+                break;
+            }
+
+        }
+        System.out.println(retext);
+    }
+}
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by nick on 13.10.16.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        for(int i=0; i<3;i++)
+        System.out.println("Hello world !!11");
+    }
+}
